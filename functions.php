61b1a94dac6cd407a25e7097d48cce2bac810096
acopiak <aaroncopiak1@gmail.com>
2020-05-18 11:18:14 -0500

    added style-editor and tested

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * aSite functions and definitions
  *
@@ -7,12 +8,12 @@
  * @package aSite
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
-if ( ! function_exists( 'asite_setup' ) ) :
+if (!function_exists('asite_setup')) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -20,17 +21,18 @@ if ( ! function_exists( 'asite_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function asite_setup() {
+	function asite_setup()
+	{
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on aSite, use a find and replace
 		 * to change 'asite' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'asite', get_template_directory() . '/languages' );
+		load_theme_textdomain('asite', get_template_directory() . '/languages');
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
 		 * Let WordPress manage the document title.
@@ -38,19 +40,19 @@ if ( ! function_exists( 'asite_setup' ) ) :
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', 'asite' ),
+				'menu-1' => esc_html__('Primary', 'asite'),
 			)
 		);
 
@@ -84,7 +86,7 @@ if ( ! function_exists( 'asite_setup' ) ) :
 		);
 
 		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support('customize-selective-refresh-widgets');
 
 		/**
 		 * Add support for core custom logo.
@@ -102,7 +104,7 @@ if ( ! function_exists( 'asite_setup' ) ) :
 		);
 	}
 endif;
-add_action( 'after_setup_theme', 'asite_setup' );
+add_action('after_setup_theme', 'asite_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -111,25 +113,27 @@ add_action( 'after_setup_theme', 'asite_setup' );
  *
  * @global int $content_width
  */
-function asite_content_width() {
+function asite_content_width()
+{
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'asite_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters('asite_content_width', 640);
 }
-add_action( 'after_setup_theme', 'asite_content_width', 0 );
+add_action('after_setup_theme', 'asite_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function asite_widgets_init() {
+function asite_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'asite' ),
+			'name'          => esc_html__('Sidebar', 'asite'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'asite' ),
+			'description'   => esc_html__('Add widgets here.', 'asite'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -137,24 +141,26 @@ function asite_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'asite_widgets_init' );
+add_action('widgets_init', 'asite_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function asite_scripts() {
-	wp_enqueue_style( 'asite-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'asite-style', 'rtl', 'replace' );
+function asite_scripts()
+{
+	wp_enqueue_style('asite-style-editor', get_template_directory_uri() . '/style-editor.css', array(),);
+	wp_enqueue_style('asite-style', get_stylesheet_uri(), array(),);
+	wp_style_add_data('asite-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'asite-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('asite-navigation', get_template_directory_uri() . '/js/navigation.js', array(), true);
 
-	wp_enqueue_script( 'asite-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
+	wp_enqueue_script('asite-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'asite_scripts' );
+add_action('wp_enqueue_scripts', 'asite_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -179,7 +185,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-

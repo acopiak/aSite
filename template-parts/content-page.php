ae938c1eb19ca959ca77190c1d1abda77701f470
acopiak <aaroncopiak1@gmail.com>
2020-05-18 19:53:27 -0500

    Finished template parts

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,22 +12,22 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry__header">
-		<?php the_title('<h1 class="entry__title">', '</h1>'); ?>
+		<h1 class="entry__title">
+			<?php the_title(); ?>
+		</h1>
 	</header><!-- .entry__header -->
 
 	<?php asite_post_thumbnail(); ?>
 
 	<div class="entry__content">
-		<?php
-		the_content();
+		<?php the_content(); ?>
 
-		wp_link_pages(
+		<?php wp_link_pages(
 			array(
 				'before' => '<div class="page__links">' . esc_html__('Pages:', 'asite'),
 				'after'  => '</div>',
 			)
-		);
-		?>
+		); ?>
 	</div><!-- .entry__content -->
 
 	<?php if (get_edit_post_link()) : ?>
